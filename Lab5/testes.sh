2163879b338f412a0c84492ed7b5b4a031571f3b
Gianluigi Dal Toso <gianluigi.toso@gmail.com>
2020-08-17 17:59:21 -0300

    Rename scripts

--- Lab5/testes.sh
+++ Lab5/testes.sh
@@ -3,10 +3,10 @@
 flex lexico.l
 yacc sintatico-semantico.y
 gcc y.tab.c main.c yyerror.c -o sintatico-semantico -lfl
-./sintatico-semantico < exemplos/testeCondicional.comp > resultados/testeCondicional.comp
-./sintatico-semantico < exemplos/testeSubscrito.comp > resultados/testeSubscrito.comp
-./sintatico-semantico < exemplos/testeExpressoes.comp > resultados/testeExpressoes.comp
-./sintatico-semantico < exemplos/testePara.comp > resultados/testePara.comp
-./sintatico-semantico < exemplos/testeLerEscrever.comp > resultados/testeLerEscrever.comp
-./sintatico-semantico < exemplos/testeEnquantoRepetir.comp > resultados/testeEnquantoRepetir.comp
-./sintatico-semantico < exemplos/testeCompleto.comp > resultados/testeCompleto.comp
+./sintatico-semantico < exemplos/testeCondicional.comp > resultados/testeCondicional.dat
+./sintatico-semantico < exemplos/testeSubscrito.comp > resultados/testeSubscrito.dat
+./sintatico-semantico < exemplos/testeExpressoes.comp > resultados/testeExpressoes.dat
+./sintatico-semantico < exemplos/testePara.comp > resultados/testePara.dat
+./sintatico-semantico < exemplos/testeLerEscrever.comp > resultados/testeLerEscrever.dat
+./sintatico-semantico < exemplos/testeEnquantoRepetir.comp > resultados/testeEnquantoRepetir.dat
+./sintatico-semantico < exemplos/testeCompleto.comp > resultados/testeCompleto.dat
