c5c0d0350b108c674f68bb92c1204e86f39f639f
lucasbeto <skybilobran@gmail.com>
2020-08-17 16:36:52 -0300

    Add testes

---
+++ Lab5/testes.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+./clean.sh
+flex lexico.l
+yacc sintatico-semantico.y
+gcc y.tab.c main.c yyerror.c -o sintatico-semantico -lfl
+./sintatico-semantico < exemplos/testeCondicional.comp > resultados/testeCondicional.comp
+./sintatico-semantico < exemplos/testeSubscrito.comp > resultados/testeSubscrito.comp
+./sintatico-semantico < exemplos/testeExpressoes.comp > resultados/testeExpressoes.comp
