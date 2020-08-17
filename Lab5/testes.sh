1a8c3af52bb15e08e5d99ba5d6d69c73872d1c8d
lucasbeto <skybilobran@gmail.com>
2020-08-17 16:52:59 -0300

    Add para test

--- Lab5/testes.sh
+++ Lab5/testes.sh
@@ -6,3 +6,4 @@ gcc y.tab.c main.c yyerror.c -o sintatico-semantico -lfl
 ./sintatico-semantico < exemplos/testeCondicional.comp > resultados/testeCondicional.comp
 ./sintatico-semantico < exemplos/testeSubscrito.comp > resultados/testeSubscrito.comp
 ./sintatico-semantico < exemplos/testeExpressoes.comp > resultados/testeExpressoes.comp
+./sintatico-semantico < exemplos/testePara.comp > resultados/testePara.comp
